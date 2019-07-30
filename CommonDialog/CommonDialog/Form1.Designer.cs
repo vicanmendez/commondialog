8021b0876c13bb267596682f23b9def36ab337c4
Víctor <44274347+vicanmendez@users.noreply.github.com>
2019-07-30 19:11:10 -0300

    primer versión
    Primer acercamiento a la clase CommonDialog y sus componentes principales. De momento copypaste de Internet, proyecto creado como ejercicio educativo para reutilizar y modificar

---
+++ CommonDialog/CommonDialog/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace CommonDialog
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.listBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(190, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "ColorDialog";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(17, 13);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(142, 20);
+            this.label.TabIndex = 1;
+            this.label.Text = "Diálogos con VS";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(190, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "FileDialog";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(190, 175);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "FolderBrowserDialog";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(190, 234);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "FontDialog";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(190, 294);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 23);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "PageSetupDialog";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // listBox
+            // 
+            this.listBox.FormattingEnabled = true;
+            this.listBox.Location = new System.Drawing.Point(12, 354);
+            this.listBox.Name = "listBox";
+            this.listBox.Size = new System.Drawing.Size(120, 95);
+            this.listBox.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 335);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Configuración de página:";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(190, 354);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(112, 24);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "PrintDialog";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 473);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Dialogos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ListBox listBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ CommonDialog/CommonDialog/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CommonDialog
+{
+    public partial class Form1 : Form
+    {
+        // Objetos PrintDocument para utilizar en el PrintDialog
+        private System.Drawing.Printing.PrintDocument docToPrint =
+            new System.Drawing.Printing.PrintDocument();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //ColorDialog
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog MyDialog = new ColorDialog();
+            // Keeps the user from selecting a custom color.
+            MyDialog.AllowFullOpen = false;
+            // Allows the user to get help. (The default is false.)
+            MyDialog.ShowHelp = true;
+            // Sets the initial color select to the current text color.
+            MyDialog.Color = label.ForeColor;
+           
+            // Update the text box color if the user clicks OK 
+            if (MyDialog.ShowDialog() == DialogResult.OK)
+                label.ForeColor = MyDialog.Color;
+        }
+
+        //FileDialog
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var fileContent = string.Empty;
+            var filePath = string.Empty;
+
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+                    }
+                }
+            }
+
+            MessageBox.Show(fileContent, "Contenido del archivo: " + filePath, MessageBoxButtons.OK);
+        }
+
+        //FolderBrowserDialog
+        private void button3_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialogExampleForm fbd = new FolderBrowserDialogExampleForm();
+            fbd.Visible = true;
+        }
+
+
+        //FontDialog
+        private void button4_Click(object sender, EventArgs e)
+        {
+            FontDialog fontDialog1 = new FontDialog(); 
+            fontDialog1.ShowColor = true;
+            fontDialog1.Font = label.Font;
+            fontDialog1.Color = label.ForeColor;
+
+            if (fontDialog1.ShowDialog() != DialogResult.Cancel)
+            {
+                label.Font = fontDialog1.Font;
+                label.ForeColor = fontDialog1.Color;
+            }
+
+        }
+
+        //PageSetupDialog
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //Creamos el objeto
+            PageSetupDialog PageSetupDialog1 = new PageSetupDialog();
+            // Initialize the dialog's PrinterSettings property to hold user
+            // defined printer settings.
+            PageSetupDialog1.PageSettings =
+                new System.Drawing.Printing.PageSettings();
+
+            // Initialize dialog's PrinterSettings property to hold user
+            // set printer settings.
+            PageSetupDialog1.PrinterSettings =
+                new System.Drawing.Printing.PrinterSettings();
+
+            //Do not show the network in the printer dialog.
+            PageSetupDialog1.ShowNetwork = false;
+
+            //Show the dialog storing the result.
+            DialogResult result = PageSetupDialog1.ShowDialog();
+
+            // If the result is OK, display selected settings in
+            // ListBox1. These values can be used when printing the
+            // document.
+            if (result == DialogResult.OK)
+            {
+                object[] results = new object[]{
+            PageSetupDialog1.PageSettings.Margins,
+            PageSetupDialog1.PageSettings.PaperSize,
+            PageSetupDialog1.PageSettings.Landscape,
+            PageSetupDialog1.PrinterSettings.PrinterName,
+            PageSetupDialog1.PrinterSettings.PrintRange};
+            listBox.Items.AddRange(results);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            //Crear objeto PrintDialog
+            PrintDialog PrintDialog1 = new PrintDialog();
+            // Allow the user to choose the page range he or she would
+            // like to print.
+            PrintDialog1.AllowSomePages = true;
+
+            // Show the help button.
+            PrintDialog1.ShowHelp = true;
+
+            // Set the Document property to the PrintDocument for 
+            // which the PrintPage Event has been handled. To display the
+            // dialog, either this property or the PrinterSettings property 
+            // must be set 
+
+            
+            PrintDialog1.Document = docToPrint;
+
+            DialogResult result = PrintDialog1.ShowDialog();
+
+            // If the result is OK then print the document.
+            if (result == DialogResult.OK)
+            {
+                docToPrint.Print();
+            }
+        }
+
+
+        /* 
+         El objeto PrintDialog imprimirá el documento a través de un evento PrintPage
+         */
+        private void document_PrintPage(object sender,
+            System.Drawing.Printing.PrintPageEventArgs e)
+        {
+
+            // Insert code to render the page here.
+            // This code will be called when the control is drawn.
+
+            // The following code will render a simple
+            // message on the printed document.
+            string text = "Texto a imprimir";
+            System.Drawing.Font printFont = new System.Drawing.Font
+                ("Arial", 35, System.Drawing.FontStyle.Regular);
+
+            // Draw the content.
+            e.Graphics.DrawString(text, printFont,
+                System.Drawing.Brushes.Black, 10, 10);
+        }
+
+    }
+}
